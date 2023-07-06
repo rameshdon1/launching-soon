960ba3b1386e30e05f3b4579a907c9bf0b93f685
Samyog Dhital <dhitalsamyog123@gmail.com>
2023-07-07 01:29:06 +0545

    chore: adds email marketing

---
+++ track.js
@@ -0,0 +1,64 @@
+const submitForm = async () => {
+  const full_name = document.querySelector('input[name="FNAME"]').value;
+  const email = document.querySelector('input[name="EMAIL"]').value;
+
+  const finalResponse = {
+    email,
+    full_name,
+    date_time: optInTime(),
+    latitude: '',
+    longitude: '',
+    timezone: '',
+    cc: '',
+    region: '',
+  };
+
+  await fetch('http://ip-api.com/json/')
+    .then((data) => {
+      if (!data.status === 'success') {
+        throw new Error('Geo Ip Error');
+      }
+      return data.json();
+    })
+    .then((geoIp) => {
+      finalResponse['latitude'] = geoIp.lat;
+      finalResponse['longitude'] = geoIp.lon;
+      finalResponse['timezone'] = geoIp.timezone;
+      finalResponse['cc'] = geoIp.countryCode;
+      finalResponse['region'] = geoIp.region;
+    })
+    .finally(() => sendEmail(finalResponse));
+};
+
+function sendEmail(data) {
+  return fetch('https://kind-jade-chipmunk-suit.cyclic.app/email', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(data),
+  });
+}
+
+function optInTime() {
+  const currentDate = new Date();
+  const month = currentDate.getMonth() + 1;
+  const day = currentDate.getDate();
+  const year = currentDate.getFullYear();
+  const hours = currentDate.getHours();
+  const minutes = currentDate.getMinutes();
+  const seconds = currentDate.getSeconds();
+
+  return (formattedDate =
+    month +
+    '/' +
+    day +
+    '/' +
+    year +
+    ' ' +
+    hours +
+    ':' +
+    minutes +
+    ':' +
+    seconds);
+}
